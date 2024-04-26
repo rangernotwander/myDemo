c573d302ac4e40cd4046bfa76253ea8258a99fde
rangernotwander <cooper163php@gmail.com>
2024-04-26 19:11:35 +0800

    Initial Commit

---
+++ myDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myDemo
+//
+//  Created by cooper Lee on 2024/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myDemo/myDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  myDemoApp.swift
+//  myDemo
+//
+//  Created by cooper Lee on 2024/4/26.
+//
+
+import SwiftUI
+
+@main
+struct myDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
